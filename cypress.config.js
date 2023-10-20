f499722896afc721a57120a71eda7ca25cc68d21
Michiel Gerritsen <michiel@controlaltdelete.nl>
2023-10-20 09:37:19 +0200

    Added Github Actions to add Cypress tests

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,26 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
+const fs = require('fs');
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:8002',
-	  supportFile:false
-  }
-})
+    supportFile: false,
+    setupNodeEvents(on, config) {
+        on('after:spec', (spec, results) => {
+            // If a retry failed, save the video, otherwise delete it to save time by not compressing it.
+            if (results && results.video) {
+                // Do we have failures for any retry attempts?
+                const failures = results.tests.find(test => {
+                    return test.attempts.find(attempt => {
+                        return attempt.state === 'failed'
+                    })
+                });
+                // Delete the video if the spec passed on all attempts
+                if (!failures) {
+                    fs.existsSync(results.video) && fs.unlinkSync(results.video)
+                }
+            }
+        })
+    },
+  },
+});
--- cypress/e2e/test/index.cy.js
+++ cypress/e2e/test/index.cy.js
@@ -2,4 +2,10 @@ describe('My First Test', () => {
   it('Visits Homepage', () => {
     cy.visit('/')
   })
+
+  it('Can visit the cart', () => {
+    cy.visit('/checkout/cart')
+
+    cy.contains('You have no items in your shopping cart.').should('be.visibile');
+  })
 })
